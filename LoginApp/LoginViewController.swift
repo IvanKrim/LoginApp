4a0f65f41a54e6e229215a3bec1906c51c27bca5
JohnKrim <reamonn96@gmail.com>
2021-05-13 15:55:46 +0300

    alertController added

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -5,6 +5,15 @@
 //  Created by Kalabishka Ivan on 13.05.2021.
 //
 
+// MARK: - IB Outlets
+// MARK: - Public Properties
+// MARK: - Private Properties
+// MARK: - Initializers
+// MARK: - Override Methods
+// MARK: - IB Action
+// MARK: - Public Methods
+// MARK: - Private Methods
+
 import UIKit
 
 class LoginViewController: UIViewController {
@@ -12,14 +21,34 @@ class LoginViewController: UIViewController {
     @IBOutlet var userTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
+    private let user = "User"
+    private let password = "123321"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-    @IBAction func loginPressed(_ sender: Any) {
+    
+    @IBAction func logInPressed() {
+        
+    }
+    
+    @IBAction func forgotButtonsPressed(_ sender: UIButton) {
+        sender.tag == 0
+            ? showAlert(title: "Forgot your username?", message: "Your login is \(user)" )
+            : showAlert(title: "Forgot your password?", message: "Your password is \(password)")
     }
     
 }
 
+extension LoginViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
