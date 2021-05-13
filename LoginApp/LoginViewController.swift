59e414985c5a8d0d714918823c4f89f7953afd74
JohnKrim <reamonn96@gmail.com>
2021-05-13 15:36:17 +0300

    Storyboards added

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -7,13 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    @IBOutlet var userTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginPressed(_ sender: Any) {
+    }
+    
 }
 
